b222021cdb138c8eaf663b373940a88af49d989a
EugeniaPC <eva290886@gmail.com>
2023-12-14 21:47:46 -0500

    day7_windows

--- src/test/java/com/loop/test/utilities/DocuportConstants.java
+++ src/test/java/com/loop/test/utilities/DocuportConstants.java
@@ -2,11 +2,23 @@ package com.loop.test.utilities;
 
 public class DocuportConstants {
 
-    public static final String USERNAME_CLIENT = "b1g1_client@gmail.com";
+    // roles
+    public static final String ADVISOR = "advisor";
+    public static final String CLIENT = "client";
+    public static final String SUPERVISOR = "supervisor";
+    public static final String EMPLOYEE = "employee";
+
+    public static final String USERNAME_CLIENT = "b1g2_client@gmail.com";
+    public static final String PASSWORD_CLIENT = "Group2";
 
 
     public static final String USERNAME_ADVISOR = "b1g2_advisor@gmail.com";
     public static final String PASSWORD_ADVISOR = "Group2";
+    public static final String USERNAME_SUPERVISOR = "b1g2_supervisor@gmail.com";
+    public static final String PASSWORD_SUPERVISOR = "Group2";
+    public static final String USERNAME_EMPLOYEE = "b1g2_employee@gmail.com";
+    public static final String PASSWORD_EMPLOYEE = "Group2";
+
 
     public static final String ERROR_MESSAGE_FOR_EMPTY_PASSWORD = "Please enter your password";
 
